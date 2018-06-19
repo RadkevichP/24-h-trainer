dad842f7d7c84d008d30b01c0ff3de80015bbac0
Pavel Radkevich <radkevichp@gmail.com>
2018-06-19 21:31:19 +0300

    fineshed lesson 2

---
+++ Hello/src/com/practicaljava/lesson2/Sale.java
@@ -0,0 +1,11 @@
+package com.practicaljava.lesson2;
+
+public class Sale {
+
+	public static void main(String[] args) {
+		System.out.println("Sales 30% at the Vitalur store!");
+		System.out.println("Hurry up!");
+
+	}
+
+}
