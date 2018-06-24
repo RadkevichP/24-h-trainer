e7f6a7db0e9ae98b3d3ffb205aa95881eabfea3d
Pavel Radkevich <radkevichp@gmail.com>
2018-06-24 18:41:29 +0300

    started Lesson 6

---
+++ Lesson 6/src/com/practicaljava/lesson6/Employee.java
@@ -0,0 +1,16 @@
+package com.practicaljava.lesson6;
+
+public class Employee extends Person implements Payable {
+
+	public Employee(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean increasePay(int percent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
