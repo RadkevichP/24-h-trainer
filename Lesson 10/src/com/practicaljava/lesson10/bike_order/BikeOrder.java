3463c944dcdce897e1ca075b7fbef7deabcdb426
Pavel Radkevich <radkevichp@gmail.com>
2018-07-04 23:47:51 +0300

    fixed 1 bug in BikeOrder

--- Lesson 10/src/com/practicaljava/lesson10/bike_order/BikeOrder.java
+++ Lesson 10/src/com/practicaljava/lesson10/bike_order/BikeOrder.java
@@ -9,7 +9,7 @@ public class BikeOrder {
 	}
 	//method for order validation
 	public void validateOrder(String bikeName, int quantity) throws TooManyBikesException {
-		if ((bikeName.equals("KidDream") && quantity > 40) || (bikeName.equals("JuniorArrow") && quantity < 35)
+		if ((bikeName.equals("KidDream") && quantity < 40) || (bikeName.equals("JuniorArrow") && quantity < 35)
 				|| (bikeName.equals("TinyLady") && quantity < 30) || (bikeName.equals("KingKong") && quantity < 25)) {
 			order = "Your order is accepted: " + bikeName + " : " + quantity;
 		}
