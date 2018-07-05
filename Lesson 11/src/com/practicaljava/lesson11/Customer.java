5499713c9317c1f65a7b4ccb15a7a118b8ad17b2
Pavel Radkevich <radkevichp@gmail.com>
2018-07-05 22:38:25 +0300

    complete the lesson 11

---
+++ Lesson 11/src/com/practicaljava/lesson11/Customer.java
@@ -0,0 +1,6 @@
+package com.practicaljava.lesson11;
+
+
+public class Customer {
+    public Customer (String a, String b){}
+}
---
+++ Lesson 11/src/com/practicaljava/lesson11/TestHashtable.java
@@ -0,0 +1,25 @@
+package com.practicaljava.lesson11;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class TestHashtable {
+	
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args){
+		
+		Customer customer = new Customer("David", "Lee");
+		Order order = new Order();
+		Portfolio portfolio = new Portfolio();
+		
+		Map data = new Hashtable();
+		data.put("Customer", customer);
+		data.put("Order", order);
+		data.put("Portfolio", portfolio);
+		
+		// retrieving the instance of Order by the key "Order"
+		Order myOrder = (Order) data.get("Order");
+		System.out.println(myOrder);
+	}
+
+}
